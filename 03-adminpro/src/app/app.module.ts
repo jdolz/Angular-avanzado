cea5941ed6fce3fce895bbd10bde46798b01855d
jdolz <j.dolz6549@gmail.com>
2021-04-30 19:27:17 +0200

    v 4

--- 03-adminpro/src/app/app.module.ts
+++ 03-adminpro/src/app/app.module.ts
@@ -6,9 +6,9 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumpsComponent } from './share/breadcrumps/breadcrumps.component';
-import { HeaderComponent } from './share/header/header.component';
-import { SidebarComponent } from './share/sidebar/sidebar.component';
+import { BreadcrumpsComponent } from './shared/breadcrumps/breadcrumps.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
