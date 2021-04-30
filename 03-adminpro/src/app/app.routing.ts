7db4ce3adf8c897f16dd83b921884d9df4adbd17
jdolz <j.dolz6549@gmail.com>
2021-04-30 23:35:08 +0200

    seccion 4 y 5 finalizadas

--- 03-adminpro/src/app/app.routing.ts
+++ 03-adminpro/src/app/app.routing.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
-
 import { Routes, RouterModule } from '@angular/router';
+import { AuthRouting } from './auth/auth.routing';
 
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 import { PagesRoutingModule } from './pages/pages.routing';
 
 const routes: Routes = [
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: NopagefoundComponent }
 ];
@@ -18,7 +15,8 @@ const routes: Routes = [
   declarations: [],
   imports: [
     RouterModule.forRoot(routes),
-    PagesRoutingModule
+    PagesRoutingModule,
+    AuthRouting
   ],
   exports: [
     RouterModule
