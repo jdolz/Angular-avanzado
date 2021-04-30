5d5b4b5285f533130755fcf84a7dbd291a28243a
jdolz <j.dolz6549@gmail.com>
2021-04-30 18:45:18 +0200

    v 2

--- 03-adminpro/src/app/app.module.ts
+++ 03-adminpro/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumpsComponent } from './share/breadcrumps/breadcrumps.component';
+import { HeaderComponent } from './share/header/header.component';
+import { SidebarComponent } from './share/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    BreadcrumpsComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
