2077966d65bc51d2a2c4277ae45a51f789d2b50f
jdolz <j.dolz6549@gmail.com>
2021-05-01 21:27:26 +0200

    clase 65

---
+++ 03-adminpro/src/app/pages/account-settings/account-settings.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-settings',
+  templateUrl: './account-settings.component.html',
+  styles: [
+  ]
+})
+export class AccountSettingsComponent implements OnInit {
+
+  linkTheme = document.querySelector('#theme');
+  links;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
+  }
+
+  changeTheme(theme: string) {
+    const url = `./assets/css/colors/${theme}.css`;
+    this.linkTheme.setAttribute('href', url);
+    localStorage.setItem('theme', url);
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+
+    this.links.forEach(element => {
+      element.classList.remove('working');
+      const btnTheme = element.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme.getAttribute('href');
+
+      if (btnThemeUrl === currentTheme) {
+        element.classList.add('working');
+      }
+    });
+
+  }
+}
