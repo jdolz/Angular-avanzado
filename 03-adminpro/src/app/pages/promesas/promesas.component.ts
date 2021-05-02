fbf266ea1972b2b99df594b2d320dbf9948fef21
jdolz <j.dolz6549@gmail.com>
2021-05-02 14:16:49 +0200

    hasta promises

---
+++ 03-adminpro/src/app/pages/promesas/promesas.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promesas',
+  templateUrl: './promesas.component.html',
+  styles: [
+  ]
+})
+export class PromesasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.getUsuarios().then(usuarios => console.log(usuarios));
+    // const promesa = new Promise((resolve, reject) => {
+    //   if (false) {
+    //     resolve('Hey');
+    //   } else {
+    //     reject('Algo fue mal');
+    //   }
+    // })
+    //   .then((mensaje) => {
+    //     console.log(mensaje);
+    //   }).catch(err => {
+    //     console.log(`La verdad es que ${err}`);
+    //   });
+
+    // console.log('Wow');
+
+  }
+
+  getUsuarios() {
+    return new Promise(resolve => {
+      fetch('https://reqres.in/api/users')
+        .then(res => res.json())
+        .then(body => resolve(body.data));
+    });
+  }
+
+}
