1988c268e6ba60a213aa6bd47a98c7d6671d8b6e
jdolz <j.dolz6549@gmail.com>
2021-05-19 20:47:06 +0200

    incrementador mejorado

--- 03-adminpro/src/app/components/incrementador/incrementador.component.ts
+++ 03-adminpro/src/app/components/incrementador/incrementador.component.ts
@@ -49,4 +49,16 @@ export class IncrementadorComponent implements OnInit {
     this.valorSalida.emit(this.progreso);
   }
 
+  checkValue(valor: number){
+    if (valor >= 100) {
+      this.progreso = 100;
+    }
+    else if (valor <= 0) {
+      this.progreso = 0;
+    }
+    else {
+      this.progreso = valor;
+    }
+  }
+
 }
