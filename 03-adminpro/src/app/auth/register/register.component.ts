3343656be28a9b1c1d306ad90427abcd71e7ff58
jdolz <j.dolz6549@gmail.com>
2021-06-18 22:02:16 +0200

    password required & match validation

--- 03-adminpro/src/app/auth/register/register.component.ts
+++ 03-adminpro/src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -15,17 +15,30 @@ export class RegisterComponent {
     password: ['', Validators.required],
     password2: ['', Validators.required],
     terms: [false, Validators.requiredTrue]
-  });
+  }, {
+    validators: this.passwordMatch('password', 'password2')
+  } as AbstractControlOptions
+  );
 
   constructor(private fb: FormBuilder) { }
 
   createUser() {
     this.formSubmitted = true;
-    console.log(this.registerForm.value);
+    console.log(this.registerForm);
+
+    if (this.registerForm.valid) console.log('Posteando');
+    else console.log('Formulario con errores');
 
   }
 
   checkInput(input: string): boolean {
     return this.registerForm.get(input).invalid && this.formSubmitted ? true : false;
   }
+
+  passwordMatch(pass1: string, pass2: string) {
+    return (formGroup: FormGroup) => {
+      formGroup.controls[pass1].value === formGroup.get(pass2).value ? formGroup.get(pass2).setErrors(null) : formGroup.get(pass2).setErrors({ notEqual: true });
+    }
+  }
+
 }
